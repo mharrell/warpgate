8fc7449facff7b11a5e20e498a76571ca9fccc35
Mike Harrell <mharrell@digitalguardian.com>
2017-12-04 15:56:45 -0700

    tests pass and fail without throwing exception

---
+++ src/test/java/Common/logResults.java
@@ -0,0 +1,17 @@
+package Common;
+
+public class logResults {
+
+
+    public static void logFailure(String message){
+        System.out.println("FAILED :(");
+//        System.out.println(message);
+    }
+
+    public static void logPassing(String message){
+        System.out.println("PASSED :)");
+//        System.out.println(message);
+    }
+
+
+}
--- src/test/java/Common/testSetup.java
+++ src/test/java/Common/testSetup.java
@@ -1,28 +1,15 @@
 package Common;
 
-//import io.restassured.RestAssured;
-//import org.apache.commons.httpclient.HttpMethod;
-//import org.apache.commons.httpclient.methods.GetMethod;
-//import org.testng.Assert;
-//import org.testng.annotations.BeforeTest;
-//import org.testng.annotations.Test;
-//import io.restassured.RestAssured.*;
-//import io.restassured.matcher.RestAssuredMatchers.*;
-//
-//import static io.restassured.RestAssured.get;
-//import static io.restassured.RestAssured.given;
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.testng.Assert.*;
-
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
-
 import java.io.File;
 import java.io.IOException;
 import java.lang.System;
 import java.util.List;
-
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import static io.restassured.RestAssured.get;
 import static org.hamcrest.core.IsEqual.equalTo;
 
@@ -37,9 +24,6 @@ public class testSetup {
         //TODO: Figure out how to just use the relative path.
         File folder = new File("/Users/mharrell/IdeaProjects/warpgate/src/test/java/TestCases");
 
-        System.out.println("Path exists: " + folder.exists());
-        System.out.println("Path is directory: " + folder.isDirectory());
-        System.out.println("Path exists: " + folder.canRead());
 
         File[] listOfFiles = folder.listFiles();
         ObjectMapper objectMapper = new ObjectMapper();
@@ -55,18 +39,15 @@ public class testSetup {
                 for (int j = 0; j < directories.length; j++) {
                     if (directories[j].isFile()) {
 
-                        System.out.println("File " + directories[j].getName());
-                        System.out.println("File " + directories[j].getPath());
 
                         try {
                             testCase testCase = objectMapper.readValue(new File(directories[j].getPath()), testCase.class);
 
-                            createTest(testCase.getUri(), testCase.getPath(), testCase.getValidation(), testCase.getTest_cases());
+                            standupTest(testCase.getUri(), testCase.getPath(), testCase.getValidation(), testCase.getTest_cases());
 
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-
 
                     } else if (directories[j].isDirectory()) {
                         System.out.println("Directory " + directories[j].getName());
@@ -78,17 +59,28 @@ public class testSetup {
         }
     }
 
-    public static void createTest(String uri, String path, String validation, List<testCase.foo> tests) {
+    public static void standupTest(String uri, String path, String validation, List<testCase.foo> tests) {
 
         for (testCase.foo foo : tests) {
             RestAssured.reset();
             RestAssured.basePath = path;
             RestAssured.baseURI = uri;
 
-//        if type "GET" then
-//            get(String.format("%s?api_key=%s", foo.getQuery(), validation)).then().log().ifValidationFails().body(foo.getResponse_route(), equalTo(foo.getExpected()));
-            get(String.format("%s?api_key=%s", foo.getQuery(), validation)).then().body(foo.getResponse_route(), equalTo(foo.getExpected()));
+                try {
+                    executeTest(validation, foo);
+                    System.out.println(String.format("PASSED: Test %s - %s", foo.getId(), foo.getDescription()));
+                }
+                catch (AssertionError assertionError) {
+                System.out.println(String.format("FAILED: Test %s - %s", foo.getId(), foo.getDescription()));
+                }
+            }
+        }
 
+    public static void executeTest(String validation, testCase.foo foo){
+
+
+//        if (foo.getType() == "GET")
+            get(String.format("%s?api_key=%s", foo.getQuery(), validation)).then().body(foo.getResponse_route(), equalTo(foo.getExpected()));
 
 //        else if type "PUT" then
 //          put(String.format("%s?api_key=%s", foo.getQuery(), validation)).then().body(foo.getResponse_route(), equalTo(foo.getExpected()));
@@ -102,4 +94,3 @@ public class testSetup {
 //        return test;
         }
     }
-}
